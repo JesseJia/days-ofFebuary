15ca859a0b610c33d9aa5479ac56711c689f82c2
JesseJia <jessejia1989@gmail.com>
2014-10-13 21:06:08 +1100

    Create daysOfFebuary.c

---
+++ daysOfFebuary.c
@@ -0,0 +1,22 @@
+#include<stdio.h> 
+void main() {   
+  int year,leap;   
+  scanf("%d",&year);  
+  if(year%4==0)  {    
+    if(year%100==0)   {     
+      if(year%400==0)     
+        leap=1;    
+      else     
+        leap=0; 
+    }   
+    else    
+      leap=1;  
+  }  
+  else   
+    leap=0;  
+  if(leap)    
+    printf("29");  
+  else    
+    printf("28"); 
+}
+  
